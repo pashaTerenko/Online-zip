08efee2cced7dd67178805c768d2d2e2a3471499
pashaTerenko <49313192+pashaTerenko@users.noreply.github.com>
2019-04-06 13:12:40 +0300

    Revert "Accounts project"
    This reverts commit c890ff2d2f4c7b20542bba95829f17946cb9c89b.

--- zip/src/main/java/com/terenko/repository/zipRepository.java
+++
@@ -1,6 +0,0 @@
-package com.terenko.repository;
-import com.terenko.model.zipFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface zipRepository extends JpaRepository<zipFile, Long> {
-
-}
